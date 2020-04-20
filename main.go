32caed844d39071e964f2195d67ade785db6c4b4
fangyuan <wojiushifangyuanlove@gmail.com>
2020-04-20 20:46:54 +0800

    括号匹配

--- main.go
+++ main.go
@@ -1,20 +1,39 @@
 package main
 
-import "fmt"
-
 type Demo struct {
 	data interface{}
 }
 
 func main() {
-	demo := Demo{}
-	//demo.data = "sdsds"
-	demo.data = 100
-	if ff, ok := demo.data.(string); ok {
-		fmt.Println("这是string")
-		fmt.Println(ff)
-	} else {
-		fmt.Println("这不是string")
-		fmt.Println(ff)
+	strArr := []byte("(()))((()))")
+	count := len(strArr)
+	cur := make([]byte, count)
+	for _, v := range strArr {
+		if getHead(cur) == v {
+
+		} else {
+		}
 	}
+}
+
+func getHead(arr []byte) byte {
+	return arr[0]
+}
+
+func popHead(arr []byte) {
+	arr = arr[1:len(arr)]
+}
+
+func popTail(arr []byte) {
+	arr = arr[0 : len(arr)-1]
+}
+
+func inArr(arr []byte, find byte) bool {
+	count := len(arr)
+	for i := 0; i < count; i++ {
+		if arr[i] == find {
+			return true
+		}
+	}
+	return false
 }
