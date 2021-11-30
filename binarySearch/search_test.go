6deed86eddbffbe6f737213187eaf4cb3b53d181
root <root@DESKTOP-08F7SQ4.localdomain>
2021-11-30 11:49:57 +0800

    update import inner package

--- binarySearch/search_test.go
+++ binarySearch/search_test.go
@@ -2,6 +2,7 @@ package binarySearch
 
 import (
 	"algorithm/sort"
+	"algorithm/utilcom/test"
 	"fmt"
 	"testing"
 )
@@ -16,58 +17,58 @@ func printTestRes(arr []int, find int, index int, f tf) {
 }
 
 func TestEasyLoopBinarySearch(t *testing.T) {
-	arr := sort.BuildTestArr(10, 20, 0)
+	arr := test.BuildTestArr(10, 20, 0)
 	find := arr[3]
 	index := -1
-	sort.Quicksort(arr);
+	sort.Quicksort(arr)
 	printTestRes(arr, find, index, EasyLoopBinarySearch)
 }
 
 func TestEasyRecursiveBinarySearch(t *testing.T) {
-	arr := sort.BuildTestArr(10, 20, 0)
+	arr := test.BuildTestArr(10, 20, 0)
 	find := arr[3]
 	index := -1
-	sort.Quicksort(arr);
+	sort.Quicksort(arr)
 	printTestRes(arr, find, index, EasyRecursiveBinarySearch)
 }
 
 func TestFindFirstEqual(t *testing.T) {
-	arr := []int{0, 0, 1, 2, 2, 3, 4, 5, 5, 5, 7, 15, 16, 19,}
+	arr := []int{0, 0, 1, 2, 2, 3, 4, 5, 5, 5, 7, 15, 16, 19}
 	find := 2
 	index := -1
 	printTestRes(arr, find, index, FindFirstEqual)
 }
 
 func TestFindEndEqual(t *testing.T) {
-	arr := []int{0, 0, 1, 2, 2, 3, 4, 5, 5, 5, 5, 5, 7, 15, 16, 19,}
+	arr := []int{0, 0, 1, 2, 2, 3, 4, 5, 5, 5, 5, 5, 7, 15, 16, 19}
 	find := 5
 	index := -1
 	printTestRes(arr, find, index, FindEndEqual)
 }
 
 func TestFindFirstGt(t *testing.T) {
-	arr := []int{4, 4, 5, 5, 6, 7, 8, 9, 9, 9, 9, 9, 12, 12, 14, 15, 16, 19,}
+	arr := []int{4, 4, 5, 5, 6, 7, 8, 9, 9, 9, 9, 9, 12, 12, 14, 15, 16, 19}
 	find := 13
 	index := -1
 	printTestRes(arr, find, index, FindFirstGe)
 }
 
 func TestFindEndGe(t *testing.T) {
-	arr := []int{4, 4, 5, 5, 8, 9, 9, 9, 9, 9, 12, 12, 14, 15, 16, 19,} //count : 16 max index 15
+	arr := []int{4, 4, 5, 5, 8, 9, 9, 9, 9, 9, 12, 12, 14, 15, 16, 19} //count : 16 max index 15
 	find := 13
 	index := -1
 	printTestRes(arr, find, index, FindEndGe)
 }
 
 func TestFindFirstLe(t *testing.T) {
-	arr := []int{4, 4, 6, 6, 8, 9, 9, 9, 9, 9, 12, 12, 14, 15, 16, 19,} //count : 16 max index 15
+	arr := []int{4, 4, 6, 6, 8, 9, 9, 9, 9, 9, 12, 12, 14, 15, 16, 19} //count : 16 max index 15
 	find := 4
 	index := -1
 	printTestRes(arr, find, index, FindFirstLe)
 }
 
 func TestFindEndLe(t *testing.T) {
-	arr := []int{4, 4, 6, 6, 8, 9, 9, 9, 9, 9, 12, 12, 14, 15, 16, 19,} //count : 16 max index 15
+	arr := []int{4, 4, 6, 6, 8, 9, 9, 9, 9, 9, 12, 12, 14, 15, 16, 19} //count : 16 max index 15
 	find := 7
 	index := -1
 	printTestRes(arr, find, index, FindEndLe)
--- fun/expr/expr_count_test.go
+++ fun/expr/expr_count_test.go
@@ -3,7 +3,6 @@ package expr
 import (
 	"fmt"
 	"math"
-	"testing"
 )
 
 type Env map[Var]float64
@@ -80,42 +79,4 @@ func (c call) Eval(env Env) float64 {
 		return math.Sqrt(c.args[0].Eval(env))
 	}
 	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
-}
-
-func TestEg1(t *testing.T) {
-	tests := []struct {
-		expr string
-		env  Env
-		want string
-	}{
-		{"sqrt(A / pi)", Env{"A": 87616, "pi": math.Pi}, "167"},
-		{"pow(x, 3) + pow(y, 3)", Env{"x": 12, "y": 1}, "1729"},
-		{"pow(x, 3) + pow(y, 3)", Env{"x": 9, "y": 10}, "1729"},
-		{"5 / 9 * (F - 32)", Env{"F": -40}, "-40"},
-		{"5 / 9 * (F - 32)", Env{"F": 32}, "0"},
-		{"5 / 9 * (F - 32)", Env{"F": 212}, "100"},
-	}
-	var prevExpr string
-	for _, test := range tests {
-		// Print expr only when it changes.
-		if test.expr != prevExpr {
-			fmt.Printf("\n%s\n", test.expr)
-			prevExpr = test.expr
-		}
-		expr, err := Parse(test.expr)
-		if err != nil {
-			t.Error(err) // parse error
-			continue
-		}
-		got := fmt.Sprintf("%.6g", expr.Eval(test.env))
-		fmt.Printf("\t%v => %s\n", test.env, got)
-		if got != test.want {
-			t.Errorf("%s.Eval() in %v = %q, want %q\n",
-				test.expr, test.env, got, test.want)
-		}
-	}
-}
-
-func Parse(expr string) (Expr, error) {
-
 }
