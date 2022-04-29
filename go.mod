4843a0dc07b374f7c2064845dd2b79a4acedfbc6
Fang Yuan <wojiushifangyuanlove@gmail.com>
2022-04-29 17:53:17 +0800

    add: New think to do
    Signed-off-by: Fang Yuan <wojiushifangyuanlove@gmail.com>

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/go-playground/webhooks/v6 v6.0.0-rc.1
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.1.0 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/go-playground/webhooks/v6/github"
+)
+
+const (
+	path = "/webhooks"
 )
 
 func main() {
-	fmt.Println(9 / 2)
+	hook, _ := github.New(github.Options.Secret("6217512Fang"))
+
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		payload, err := hook.Parse(r, github.ReleaseEvent, github.PullRequestEvent)
+		if err != nil {
+			if err == github.ErrEventNotFound {
+				fmt.Printf("%+v", err)
+			}
+		}
+		switch payload.(type) {
+
+		case github.ReleasePayload:
+			release := payload.(github.ReleasePayload)
+			// Do whatever you want from here...
+			fmt.Printf("%+v", release)
+
+		case github.PullRequestPayload:
+			pullRequest := payload.(github.PullRequestPayload)
+			// Do whatever you want from here...
+			fmt.Printf("%+v", pullRequest)
+		}
+	})
+	http.ListenAndServe(":3000", nil)
 }
